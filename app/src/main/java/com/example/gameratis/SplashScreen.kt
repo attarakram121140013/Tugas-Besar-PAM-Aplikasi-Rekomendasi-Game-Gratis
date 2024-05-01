2ae411396dd3c1b80b6f47a7d1b776d3313bde04
fariedhalim121140197 <muhammad.121140197@student.itera.ac.id>
2024-05-01 20:19:23 +0700

    Upload

--- app/src/main/java/com/example/gameratis/SplashScreen.kt
+++ app/src/main/java/com/example/gameratis/SplashScreen.kt
@@ -1,20 +1,22 @@
 package com.example.gameratis
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.gameratis.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashText.animate().setDuration(3000).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
         }
-    }
+
+        }
 }
