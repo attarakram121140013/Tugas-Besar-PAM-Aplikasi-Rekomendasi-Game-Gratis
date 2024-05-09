eb280d898cfe17ab9304b80ca77320a1d400139c
Attar Akram Abdillah <wataataru@gmail.com>
2024-05-09 19:09:38 +0700

    File Organizing

--- app/src/main/java/com/example/gameratis/ui/search/DashboardViewModel.kt
+++ app/src/main/java/com/example/gameratis/ui/search/DashboardViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.gameratis.ui.notifications
+package com.example.gameratis.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
