14f2063bfd0c6acbc0f37ed1d9e97915377a7bbb
Attar Akram Abdillah <wataataru@gmail.com>
2024-05-18 13:43:23 +0700

    Small Configuration

--- app/src/main/java/com/example/gameratis/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/gameratis/ui/favorites/FavoritesFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.gameratis.databinding.FragmentNotificationsBinding
+import com.example.gameratis.databinding.FragmentFavoritesBinding
 
-class NotificationsFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoritesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- app/src/main/java/com/example/gameratis/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/gameratis/ui/favorites/FavoritesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.gameratis.ui.search
+package com.example.gameratis.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Favorites Fragment"
     }
     val text: LiveData<String> = _text
 }
